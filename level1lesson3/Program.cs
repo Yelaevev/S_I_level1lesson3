5dd773d793f4470240bcea4f1a0567f89888ae23
Yelaevev <yelaevevgen@gmail.com>
2020-05-30 18:34:24 +0300

    remove local fuction

--- level1lesson3/Program.cs
+++ level1lesson3/Program.cs
@@ -47,86 +47,86 @@ namespace Level1Space
                 }
             }
 
-            int[,] pole1 = new int[N, M];
-            int[] zahvat(int[] desant, int[,] pole)
-            {
+                int[,] pole = new int[N, M];
+                //int[] zahvat(int[] desant, int[,] pole)
+                int[] desant = battalion;
                 int strminus, strplus, stroka;
                 int stlplus, stlminus, stolbets;
-                int[] pokolenie1 = new int[4 * desant.Length];
+                //int[] pokolenie1 = new int[4 * desant.Length];
 
-              
-                int pc = 0;
-                int i1, i2, i3, i4, j1, j2, j3, j4;
-                i1 = i2 = i3 = i4 = j1 = j2 = j3 = j4 = -1;
 
-                for (int tk = 0; tk < (desant.Length - 1); tk = tk + 2)
-                {
-                    if (desant[tk] >= 0)
-                    {
+            int pc = 0;
+            int i1, i2, i3, i4, j1, j2, j3, j4;
+            i1 = i2 = i3 = i4 = j1 = j2 = j3 = j4 = -1;
 
-                        i1 = i2 = i3 = i4 = j1 = j2 = j3 = j4 = -1;
-                        stroka = desant[tk];
-                        stolbets = desant[tk + 1];
-                        strminus = stroka;
-                        strplus = stroka;
-                        stlminus = stolbets;
-                        stlplus = stolbets;
-                        pole[stroka, stolbets] = 5;
+           
 
-                        if ((strplus < N - 1) && (pole[strplus + 1, stolbets] == 0))
-                        {
-                            pole[strplus + 1, stolbets] = pole[strplus, stolbets];
-                            i1 = strplus + 1;
-                            j1 = stolbets;
-                           
-                        }
-
-                        if ((strminus > 0) && (pole[strminus - 1, stolbets] == 0))
-                        {
-                            pole[strminus - 1, stolbets] = pole[strminus, stolbets];
-                            i2 = strminus - 1;
-                            j2 = stolbets;
-                       
-                        }
-
-                        //pole[stroka, stlplus + 1] = pole[stroka, stolbets];
-
-                        if ((stlplus < M - 1) && (pole[stroka, stlplus + 1] == 0))
-                        {
-                            pole[stroka, stlplus + 1] = pole[stroka, stolbets];
-                            i3 = stroka;
-                            j3 = stlplus + 1;
-                          
-                        }
-
-                        if ((stlminus > 0) && (pole[stroka, stlminus - 1] == 0))
-                        {
-                            pole[stroka, stlminus - 1] = pole[stroka, stolbets];
-                            i4 = stroka;
-                            j4 = stlplus - 1;
-                          
-                        }
-                        pc = 4 * tk;
-                        pokolenie1[pc] = i1; pokolenie1[pc + 2] = i2; pokolenie1[pc + 4] = i3; pokolenie1[pc + 6] = i4;
-                        pokolenie1[pc + 1] = j1; pokolenie1[pc + 3] = j2; pokolenie1[pc + 5] = j3; pokolenie1[pc + 7] = j4;
-
-                    }
-                }
-
-                return pokolenie1;
-            }
-            
             int chet = 1;
             for (int i = 0; i < N; i++)
             {
                 for (int j = 0; j < M; j++)
                 {
-                    while (pole1[i, j] == 0)
+                    while (pole[i, j] == 0)
                     {
-                        int[] c = zahvat(battalion, pole1);
-                        battalion = c;
+                        int[] pokolenie1 = new int[4 * desant.Length];
+                        for (int tk = 0; tk < (desant.Length - 1); tk = tk + 2)
+                        {
+                            if (desant[tk] >= 0)
+                            {
+
+
+                                i1 = i2 = i3 = i4 = j1 = j2 = j3 = j4 = -1;
+                                stroka = desant[tk];
+                                stolbets = desant[tk + 1];
+                                strminus = stroka;
+                                strplus = stroka;
+                                stlminus = stolbets;
+                                stlplus = stolbets;
+                                pole[stroka, stolbets] = 5;
+
+                                if ((strplus < N - 1) && (pole[strplus + 1, stolbets] == 0))
+                                {
+                                    pole[strplus + 1, stolbets] = pole[strplus, stolbets];
+                                    i1 = strplus + 1;
+                                    j1 = stolbets;
+
+                                }
+
+                                if ((strminus > 0) && (pole[strminus - 1, stolbets] == 0))
+                                {
+                                    pole[strminus - 1, stolbets] = pole[strminus, stolbets];
+                                    i2 = strminus - 1;
+                                    j2 = stolbets;
+
+                                }
+
+                                //pole[stroka, stlplus + 1] = pole[stroka, stolbets];
+
+                                if ((stlplus < M - 1) && (pole[stroka, stlplus + 1] == 0))
+                                {
+                                    pole[stroka, stlplus + 1] = pole[stroka, stolbets];
+                                    i3 = stroka;
+                                    j3 = stlplus + 1;
+
+                                }
+
+                                if ((stlminus > 0) && (pole[stroka, stlminus - 1] == 0))
+                                {
+                                    pole[stroka, stlminus - 1] = pole[stroka, stolbets];
+                                    i4 = stroka;
+                                    j4 = stlplus - 1;
+
+                                }
+                                pc = 4 * tk;
+                                pokolenie1[pc] = i1; pokolenie1[pc + 2] = i2; pokolenie1[pc + 4] = i3; pokolenie1[pc + 6] = i4;
+                                pokolenie1[pc + 1] = j1; pokolenie1[pc + 3] = j2; pokolenie1[pc + 5] = j3; pokolenie1[pc + 7] = j4;
+
+                            }
+                        }
+                        // int[] c = zahvat(battalion, pole1);
+                        desant = pokolenie1;
                         chet = chet + 1;
-                       
+
                     }
                 }
             }
@@ -136,14 +136,24 @@ namespace Level1Space
         //{
         //    int N = 5;
         //    int M = 7;
-         
-        //    int[,] pole1 = new int[N, M];
-        //    int chet;
+        //    int[,] pole = new int[N, M];
+        //    int chet1;
+        //    int[] test = { 5,1, 1,7 };
+        //    int[] desant = test;
+        //    int strminus, strplus, stroka;
+        //    int stlplus, stlminus, stolbets;
 
-         
-        //        int[] test = { 3, 4, 1,4};
-        //    chet = ConquestCampaign(3, 4, 2, test);
-        //    Console.WriteLine($"znachenie chet= {chet}");
+        //    //int[] pokolenie1 = new int[4 * desant.Length];
+
+        //    int pc = 0;
+        //    int i1, i2, i3, i4, j1, j2, j3, j4;
+        //    i1 = i2 = i3 = i4 = j1 = j2 = j3 = j4 = -1;
+
+           
+
+
+        //    chet1 = ConquestCampaign(5, 7, 2, test);
+        //    Console.WriteLine(chet1);
             //for (int n = 0; n < N; n++)
             //{
             //    for (int m = 0; m < M; m++)
@@ -152,9 +162,9 @@ namespace Level1Space
             //    }
             //    Console.WriteLine("");
             //}
-
             //  Console.WriteLine($"znachenie chet= {chet}");
+           // }
 
-        //}
+       
     }
 }
